ab3e3b48c98651f57678d9b13448f65b93416bbd
Tsuchijo <TsuchitoriJoel@gmail.com>
2023-01-30 18:28:35 -0800

    updated readme and made it use resume file instead of paste

--- CoverLetterGen.py
+++ CoverLetterGen.py
@@ -7,14 +7,16 @@ from Loader import Loader
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-description_path = "description/job_desc"
 prompt_path = "prompt"
 prompt = open(prompt_path, "r")
+resume_path = "resume/resume.txt"
 
+## Function to clear terminal
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-#Function to read multiple lines from user
+## Function to read multiple lines from user
+# @param statement: statement to print before reading
 def multi_read(statement):
     print(statement + "(Ctrl-D or Ctrl-Z ( windows ) to save)")
     lines = ""
@@ -25,53 +27,78 @@ def multi_read(statement):
             return lines
         lines = lines + (line)
 
-def generate_prompt():
-    if(not os.path.exists("skills/summary")):
-        print("no resume given!")
-        format_resume(input("input resume text:"))
-    description = open(description_path, "r").read()
-    summary = open("skills/summary", "r")
+## Function to generate prompt for cover letter
+# @param description: job description to use as prompt
+def generate_prompt(description):
+    #check if resume exists
+    # if not, prompt user to input one
+    if(not os.path.exists(resume_path)):
+        print("no resume detected, add a resume to the resume folder or input text!")
+        format_resume(multi_read("input resume text:"))
+    resume = open(resume_path, "r")
+    return prompt.readline() + resume.read() + prompt.readline() + description
 
-    return prompt.readline() + summary.read() + prompt.readline() + description
-
-def generate_letter():
+## Function to generate cover letter
+# @param description: job description to use as prompt
+def generate_letter(description):
     return openai.Completion.create(
         model="text-davinci-003",
-        prompt=generate_prompt(),
+        prompt=generate_prompt(description),
         temperature=0.9,
         max_tokens=1800
     )
 
+## Formats and saves the resume to the resume folder
+# @param resume: resume text to format
 def format_resume(resume):
-    summary = open("skills/summary", "w")
-    loader = Loader("Saving resume...", "Finished", 0.05).start()
-    summary_text = openai.Completion.create(
-            model="text-davinci-003",
-            prompt= "format this resume to be easier to read and remove extraneous information: " + resume,
-            temperature=0.7,
-            max_tokens=1200,
-    ).choices[0].text
-    loader.stop()
-    summary.write(summary_text)
+    summary = open(resume_path, "w")
+    summary.write(resume)
 
-def read_input():
+def save_file(file_name, text):
+    with open(file_name + '.txt', 'w') as f:
+        f.write(text)
+
+## Function to read input from user and edit letter
+# @param description: job description to use as prompt
+def read_input(description):
+    #start generating cover letter
     loader = Loader("Generating Cover Letter...", "Finished", 0.05).start()
-    letter = generate_letter()
+    letter = generate_letter(description)
     loader.stop()
     clear()
+    #print letter
     print(letter.choices[0].text)
+
+    ## loop reading user input to edit letter
+    #quit to exit
+    #all other input is used to edit letter as queries to OpenAI
+    #@TODO: add more options
+    #      render, save, etc
     while True:
+        saved = False
         response = input(":")
         if response == 'quit':
+            #check if they have saved before quitting
+            if(not saved):
+                if(input("save before quitting? (y/n)") == 'y'):
+                    save_name = input("save as:")
+                    save_file(save_name, letter.choices[0].text)
             print('quitting...')
             exit(1)
+        if response == 'save':
+            print('saving...')
+            #ask for file name
+            save_name = input("save as:")
+            save_file(save_name, letter.choices[0].text)
+            print('saved!')
         else:
             loader = Loader("Editing...", "Finished", 0.05).start()
-            letter = openai.Completion.create(
-                model="text-davinci-003",
-                prompt= "rewrite these cover letter taking into account this feedback, then repeat the complete copy: "+ response + '\n' + letter.choices[0].text,
+            letter = openai.Edit.create(
+                model="text-davinci-edit-001",
+                input= letter.choices[0].text,
+                instructions= response,
                 temperature=0.7,
-                max_tokens= letter.usage.completion_tokens + 100 + (int)(len(response) / 6),
+                max_tokens= letter.usage.completion_tokens + 100, 
             ).choices[0].text
             loader.stop()            
             clear()
@@ -79,16 +106,10 @@ def read_input():
 
 def main():
     clear()
-    response = input("New Resume (y/N):")
-    clear()
-    if(response == 'y'):
-        resume_text = multi_read("input resume text: ")
-        format_resume(resume_text)
-    clear()
+    #prompts user to input job description 
     desc_text = multi_read("input job description:")
-    open(description_path, "w").write(desc_text)
     clear()
-    read_input()
+    read_input(desc_text)
 
 if __name__ == "__main__":
     main()
--- readme.md
+++ readme.md
@@ -1,4 +1,11 @@
 # README
-This is a basic commandline utility to generate cover letters using OpenAI's gpt 3 API.
+A basic command line utility to generate cover letters using OpenAI's gpt 3 API. Has the ability to generate a simple cover letter based ona  given resume and job description, as well as basic ai based editing capabilities (more advanced editing option TBA).
 
-# How to use
+# How to use (Linux)
+1. Download the project with: ```git clone https://github.com/Tsuchijo/CoverLetterGenerator.git```
+2. enter the directory with ```cd CoverLetterGenerator``` then create a file named ```.env```
+3. Sign into your OpenAI account then generate an API key: https://platform.openai.com/account/api-keys
+4. In the ```.env``` file paste in ```OPENAI_API_KEY=<OpenAI api key>```, inputting your OpenAI API key where the brackets are (without the brackets)
+5. Activate the environment by running the comman ```./envAI/activate```
+6. Go to the resume directory with ```cd resume``` and create a text file named ```resume.txt``` with the plain text version of your resume
+7. Finally, run the python script with ```python CoverLetterGen.py```
